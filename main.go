e792be98c7382051828bca8efea9d45875712290
Vallery Lancey <vallerylancey@gmail.com>
2020-04-26 20:29:00 -0700

    Add basic instance/directory command line flags.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 
 	"github.com/pkg/errors"
 
@@ -11,14 +12,26 @@ import (
 )
 
 func main() {
+	var directory string
 	var gcloudCredentialsPath string
+	var instance string
+
+	flag.StringVar(&directory, "directory", "", "Base directory for gameserver config and data.")
 	flag.StringVar(&gcloudCredentialsPath, "gcloud-credentials-path", "", "Path to Google Cloud credentials file. No path will fall back to the magic SDK behavior.")
+	flag.StringVar(&instance, "instance", "", "Game instance to run as. The instance represents a distinct world/save for a given game, e.g. different modpacks for Minecraft.")
 	flag.Parse()
+
+	if directory == "" {
+		log.Fatal("Must supply --directory.")
+	}
+
+	if instance == "" {
+		log.Fatal("Must supply --instance. Use --instance test for testing.")
+	}
 
 	backupManager := backup.NewManager(backup.NewGoogleCloudStorage(gcloudCredentialsPath), "/tmp")
 
-	// TODO take input.
-	g, err := game.NewGame("minecraft", "fake", "test/minecraft", backupManager)
+	g, err := game.NewGame("minecraft", instance, directory, backupManager)
 	if err != nil {
 		panic(errors.Wrap(err, "this server has ants"))
 	}
